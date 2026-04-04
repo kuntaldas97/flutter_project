a806cc5eee7f9930dfc3b09cda5b577230270e9d
Kuntal Das <122853910+kuntaldas97@users.noreply.github.com>
2026-04-04 23:53:10 +0530

    9th  commit for favourite places

--- favourite_places/lib/widgets/image_input.dart
+++ favourite_places/lib/widgets/image_input.dart
@@ -3,65 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget{
-  @override
+ const ImageInput({super.key});
+
+ @override
   State<ImageInput> createState() {
     return _ImageInputState();
   }
 }
 class _ImageInputState extends State<ImageInput>{
- void _selectImageSource() {
-  showModalBottomSheet(
-    context: context,
-    builder: (ctx) {
-      return SafeArea(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ListTile(
-              leading: Icon(Icons.camera),
-              title: Text('Camera'),
-              onTap: () {
-                Navigator.of(ctx).pop();
-                _pickImage(ImageSource.camera);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.photo),
-              title: Text('Gallery'),
-              onTap: () {
-                Navigator.of(ctx).pop();
-                _pickImage(ImageSource.gallery);
-              },
-            ),
-          ],
-        ),
-      );
-    },
-  );
-}
-void _pickImage(ImageSource source) async {
-  final imagePicker = ImagePicker();
-  File? _selectedImage;
-  final pickedImage = await imagePicker.pickImage(
-    source: source,
-    maxHeight: 600,
-  );
 
-  if (pickedImage == null) return;
-
-  // You can store or display the image here
-  _selectedImage = File(pickedImage.path);
-}
   @override
   Widget build(BuildContext context) {
-   return Container(
-    height: 250,
-    width: double.infinity,
-    alignment: Alignment.center,
-    child: TextButton.icon(
-      icon: Icon(Icons.camera),
-      onPressed: _selectImageSource, 
-      label: Text("Select Image")),
-   );
+    return Container();
   }
 }
